ba8a47a1156f84d4ca750e7d0fd0d7083f2e6ae5
ronkips <kipshillash@gmail.com>
2022-05-09 20:03:11 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function palindrome(str) {
+    const alphanumericOnly = str
+    .toLowerCase()
+     .match(/[a-z0-9]/g);
+     return alphanumericOnly.join('') ===
+          alphanumericOnly.reverse().join('');
+  }
+  
+  palindrome("eye");
